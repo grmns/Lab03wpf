01fd8b2d5e686d9b8e1c9e5e8e22605dab85a1bb
German <German@DESKTOP-OT3T8Q7>
2024-09-03 18:29:39 -0500

    Agregar archivos de proyecto.

---
+++ Lab03wpf/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab03wpf
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string connectionString = "Server=DESKTOP-OT3T8Q7; Database=Tecsup2023DB; Integrated Security=True";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadButton_Click(object sender, RoutedEventArgs e)
+        {
+            LoadData();
+        }
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            string searchQuery = searchTextBox.Text;
+            SearchDataByName(searchQuery);
+        }
+
+        private void LoadData()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    SqlCommand command = new SqlCommand("SELECT * FROM Students", connection);
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+
+                    dataGrid.ItemsSource = dataTable.DefaultView;
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Error de Conexion");
+                }
+            }
+        }
+
+        private void SearchDataByName(string name)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    SqlCommand command = new SqlCommand("SELECT * FROM Students WHERE FirstName LIKE @name", connection);
+                    command.Parameters.AddWithValue("@name", "%" + name + "%");
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+
+                    dataGrid.ItemsSource = dataTable.DefaultView;
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Error de Conexion");
+                }
+            }
+        }
+    }
+}
